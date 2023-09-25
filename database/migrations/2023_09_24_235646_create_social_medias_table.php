74e707e59820cf3e7510d878bc3ba1054c9ace6e
laiguz <laiguz.devs@hotmail.com>
2023-09-25 19:47:17 -0300

    master

---
+++ database/migrations/2023_09_24_235646_create_social_medias_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('social_medias', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('active')->nullable();
+            $table->string('title')->nullable();
+            $table->string('media')->nullable();
+            $table->string('link')->nullable();
+            $table->string('slug')->nullable();
+            $table->timestamps();
+            $table->string('updated_by',50)->nullable();
+            $table->string('created_by',50)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('social_medias');
+    }
+};
